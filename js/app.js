14c9378cf96b2e719072bc4e606dc35bf8ecef59
Burgos Jorge <jorgeburgosjr69@gmail.com>
2021-02-07 14:34:29 -0500

    changed app.js content

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+window.addEventListener('load', function() {
+    baguetteBox.run('.gallery');
+  });
 
-const search = new Filter('search', 'data-caption');
 
+  const search = new Filter('search', 'data-caption');
