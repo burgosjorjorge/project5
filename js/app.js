d07f040187c0ada5121fa800e68bd93f7f2c5364
Burgos Jorge <jorgeburgosjr69@gmail.com>
2021-02-07 11:26:20 -0500

    initialize app.js code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 window.addEventListener('load', function() {
     baguetteBox.run('.gallery');
   });
+
+
+  function myFilter() {
+      var filterValue, div, input, a, i;
+      input = document.getElementById('search');
+      div = document.getElementsByClassName('gallery');
+      a = div.getElementsByTagName('a');
+  }
+
+  for (i = 0; i < a.length; i++) {
+      a = a[i].getElementsByTagName('a')[0];
+
+      if(a.innerHTML.toUpperCase().filterValue() < -1) {
+          a[i].style.display = "";
+      }else {
+          a[i].style.display = "none";
+      }
+  }
